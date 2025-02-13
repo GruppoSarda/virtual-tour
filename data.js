2a5640a628e2fc049cd0653b1c4092861c3bba4c
GruppoSarda <andrea_pis@live.it>
2025-02-13 22:51:51 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,483 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-00-ingresso",
+      "name": "00 ingresso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.18083744104763966,
+        "pitch": 0.31803656940030045,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.526264077315245,
+          "pitch": 0.07895430094585265,
+          "rotation": 4.71238898038469,
+          "target": "1-01-camera"
+        },
+        {
+          "yaw": 0.14128725105883966,
+          "pitch": 0.1349822928825759,
+          "rotation": 6.283185307179586,
+          "target": "3-03-loggiato"
+        },
+        {
+          "yaw": -1.0391863340102638,
+          "pitch": 0.06182012334824094,
+          "rotation": 4.71238898038469,
+          "target": "2-02-lavanderia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-01-camera",
+      "name": "01 camera",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4852875167012805,
+          "pitch": -0.01928274350267145,
+          "rotation": 4.71238898038469,
+          "target": "0-00-ingresso"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-02-lavanderia",
+      "name": "02 lavanderia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4710429066910242,
+          "pitch": 0.28648633483762787,
+          "rotation": 0,
+          "target": "0-00-ingresso"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-03-loggiato",
+      "name": "03 loggiato",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.272432266348652,
+          "pitch": 0.16828150794110996,
+          "rotation": 4.71238898038469,
+          "target": "5-05-bagno"
+        },
+        {
+          "yaw": -0.9054922869227191,
+          "pitch": -0.01461024483075235,
+          "rotation": 4.71238898038469,
+          "target": "7-07-salone"
+        },
+        {
+          "yaw": 0.2870916736126077,
+          "pitch": 0.38545949819730474,
+          "rotation": 0,
+          "target": "0-00-ingresso"
+        },
+        {
+          "yaw": -2.3890786771528187,
+          "pitch": -0.03844531977810561,
+          "rotation": 1.5707963267948966,
+          "target": "4-04-camera"
+        },
+        {
+          "yaw": -2.91747269042037,
+          "pitch": -0.057583900738585214,
+          "rotation": 0,
+          "target": "8-08-giardino"
+        },
+        {
+          "yaw": -3.092987068035084,
+          "pitch": 0.05128340433444123,
+          "rotation": 4.71238898038469,
+          "target": "6-06-bagno"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-04-camera",
+      "name": "04 camera",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4211996491438406,
+          "pitch": 0.23567252113451076,
+          "rotation": 0,
+          "target": "3-03-loggiato"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-05-bagno",
+      "name": "05 bagno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.0559088715984828,
+        "pitch": 0.21038095245830668,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.032884658988884,
+          "pitch": 0.1952708172279749,
+          "rotation": 0,
+          "target": "3-03-loggiato"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-06-bagno",
+      "name": "06 bagno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.945868350467409,
+          "pitch": 0.1307148129555511,
+          "rotation": 0,
+          "target": "3-03-loggiato"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-07-salone",
+      "name": "07 salone",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4601937298361634,
+          "pitch": 0.08087642723568678,
+          "rotation": 4.71238898038469,
+          "target": "3-03-loggiato"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-08-giardino",
+      "name": "08 giardino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3455088079464836,
+          "pitch": -0.04943051769328477,
+          "rotation": 0,
+          "target": "9-09-camera"
+        },
+        {
+          "yaw": 1.9640053009913183,
+          "pitch": -0.02417244622205672,
+          "rotation": 3.141592653589793,
+          "target": "10-10-giardino"
+        },
+        {
+          "yaw": -0.20225806076508235,
+          "pitch": 0.24951777495344274,
+          "rotation": 0,
+          "target": "3-03-loggiato"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-09-camera",
+      "name": "09 camera",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.000318783010771,
+          "pitch": 0.18307176768661293,
+          "rotation": 0,
+          "target": "8-08-giardino"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-10-giardino",
+      "name": "10 giardino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2789914793622401,
+          "pitch": 0.2666864368935471,
+          "rotation": 0,
+          "target": "8-08-giardino"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
